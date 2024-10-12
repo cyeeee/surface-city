5ca80a06dbb83d1316c79673a6eccc2ecb9ebd91
fuguoxue-nyu <144282221+fuguoxue-nyu@users.noreply.github.com>
2024-10-11 21:59:26 -0400

    Added UI toggle function. Error handling tested and worked fine.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-const GRID_HEIGHT = 101/2.5;
-const GRID_WIDTH = 174/2.5; // width = height * sqrt(3)
+const GRID_HEIGHT = 101 / 2.5;
+const GRID_WIDTH = 174 / 2.5; // width = height * sqrt(3)
 let NUM_COLS;
 let NUM_ROWS;
 let ALL_TILE_IDXS;
@@ -12,14 +12,15 @@ let cityIdx = "0";
 let grid = [];
 const updateQueue = [];
 let hasUpdated = {};
-let fillerTile;
+
+let uiVisible = true;
 
 function toI(xi, yi) {
   return yi * NUM_COLS + xi;
 }
 
 function addToQueue(idx, oSize = -1) {
-  const changed = (oSize == -1) || grid[idx].possibilities.length < oSize;
+  const changed = oSize == -1 || grid[idx].possibilities.length < oSize;
 
   if (!(idx in hasUpdated) && changed) {
     updateQueue.push(idx);
@@ -78,16 +79,16 @@ function setup() {
 
   dropdownMenu = createSelect();
   dropdownMenu.position(windowWidth / 2 + 10, windowHeight - 103);
-  dropdownMenu.option('0');
-  dropdownMenu.option('1');
-  dropdownMenu.option('2');
-  dropdownMenu.option('3');
-  dropdownMenu.option('4');
-  dropdownMenu.option('39');
-  dropdownMenu.option('41');
-  dropdownMenu.option('68');
-  dropdownMenu.option('97');
-  dropdownMenu.changed(()=>{
+  dropdownMenu.option("0");
+  dropdownMenu.option("1");
+  dropdownMenu.option("2");
+  dropdownMenu.option("3");
+  dropdownMenu.option("4");
+  dropdownMenu.option("39");
+  dropdownMenu.option("41");
+  dropdownMenu.option("68");
+  dropdownMenu.option("97");
+  dropdownMenu.changed(() => {
     selectedCityIdx = dropdownMenu.selected().toString();
   });
 
@@ -102,20 +103,30 @@ function setup() {
 }
 
 function initializeTiles() {
+  // Clear the TILES array to avoid adding to the previous set
+  TILES.length = 0;
+
   // Get columns in the csv file
-  let tileIdx = table.getColumn("Tile Name");  
+  let tileIdx = table.getColumn("Tile Name");
   let tileRight = table.getColumn("Right Label");
   let tileBottom = table.getColumn("Bottom Label");
   let tileLeft = table.getColumn("Left Label");
   let tileTop = table.getColumn("Top Label");
-  let tileUsed = table.getColumn("Used - " + dropdownMenu.selected().toString()); // Change the number 68 to the district you want to see. Current available districts: 0, 1, 2, 3, 4, 39, 41, 68, 97
-  fillerTile = tileUsed[0];
+  let tileUsed = table.getColumn(
+    "Used - " + dropdownMenu.selected().toString()
+  ); // Change the number 68 to the district you want to see. Current available districts: 0, 1, 2, 3, 4, 39, 41, 68, 97
 
-  for (let i=0; i<tileIdx.length; i++) {
+  for (let i = 0; i < tileIdx.length; i++) {
     if (parseInt(tileUsed[i]) == 1) {
-      TILES.push(new Tile(drawImg(parseInt(tileIdx[i])), [parseInt(tileRight[i]),parseInt(tileBottom[i]),parseInt(tileLeft[i]),parseInt(tileTop[i])])); 
+      TILES.push(
+        new Tile(drawImg(parseInt(tileIdx[i])), [
+          parseInt(tileRight[i]),
+          parseInt(tileBottom[i]),
+          parseInt(tileLeft[i]),
+          parseInt(tileTop[i]),
+        ])
+      );
     }
-    
   }
   ALL_TILE_IDXS = TILES.map((_, i) => i);
 }
@@ -131,15 +142,33 @@ function initializeGrid() {
 
 function resetGrid() {
   initializeTiles(); // Reinitialize the TILES array
-  initializeGrid();  // Reinitialize the grid
+  initializeGrid(); // Reinitialize the grid
   cityIdx = selectedCityIdx;
   draw(); // Manually trigger draw once
+}
+
+function UIPanel() {
+  fill("rgba(255, 255, 255, 0.25)");
+  rect(width / 2 - 300, height - 300, 600, 300);
+  fill(0);
+  textFont("monospace", 40);
+  textAlign(CENTER, CENTER);
+  text("The Surface City", windowWidth / 2, windowHeight - 200);
+  textSize(30);
+  let districtName = "SC-" + cityIdx;
+  text(districtName, windowWidth / 2, windowHeight - 150);
+  textSize(20);
+  text("Change District: ", windowWidth / 2 - 80, windowHeight - 90);
+  textSize(15);
+  text("Press \"u\" key to toggle this panel", windowWidth / 2, windowHeight - 40);
 }
 
 function draw() {
   background(220, 0);
 
-  const byPossibilities = grid.toSorted((a, b) => a.possibilities.length - b.possibilities.length);
+  const byPossibilities = grid.toSorted(
+    (a, b) => a.possibilities.length - b.possibilities.length
+  );
   const candidates = byPossibilities.filter((p) => p.possibilities.length > 1);
 
   if (candidates.length > 0) {
@@ -166,7 +195,9 @@ function draw() {
       if (mTile) {
         mTile.draw(mGridy.xi, mGridy.yi, GRID_WIDTH, GRID_HEIGHT);
       } else {
-        console.error(`Error: Tile at index ${mGridy.possibilities[0]} is undefined.`);
+        console.error(
+          `Error: Tile at index ${mGridy.possibilities[0]} is undefined.`
+        );
         resetGrid();
       }
     } else {
@@ -174,16 +205,24 @@ function draw() {
       resetGrid();
     }
   }
+  if (uiVisible) {
+    UIPanel();
+  }
+}
 
-  fill("rgba(255, 255, 255, 0.25)");
-  rect(width / 2 - 300, height - 300, 600, 300);
-  fill(0);
-  textFont("monospace", 40);
-  textAlign(CENTER, CENTER);
-  text("The Surface City", windowWidth / 2, windowHeight - 200);
-  textSize(30);
-  let districtName = "SC-" + cityIdx;
-  text(districtName, windowWidth / 2, windowHeight - 150);
-  textSize(20);
-  text("Change District: ", windowWidth / 2 - 80, windowHeight - 90);
+function keyPressed() {
+  if (key == "u") {
+    uiVisible = !uiVisible; // Toggle UI visibility state
+
+    // Show or hide UI elements based on the new state
+    if (uiVisible) {
+      dropdownMenu.show();
+      buttonGenerate.show();
+    } else {
+      dropdownMenu.hide();
+      buttonGenerate.hide();
+    }
+
+    return false; // Prevent the default behavior of TAB key
+  }
 }
